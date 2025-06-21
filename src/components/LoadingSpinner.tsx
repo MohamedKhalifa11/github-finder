24dbace2a79e708222873763f1c13bdec38fc77f
Mohamed Khalifa <mkhkun11@gmail.com>
2025-06-21 12:32:19 +0300

    feat: add reusable loading component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex h-32 items-center justify-center">
+    <div className="size-16 animate-spin rounded-full border-b-2 border-gray-800"></div>
+  </div>
+);
+
+export default LoadingSpinner;
